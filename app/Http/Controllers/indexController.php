f9a707c996e423d719d0b950c39a4f4be8cf21ae
shaghayeghjavid <sh.javid09@gmail.com>
2020-02-15 20:29:57 +0330

    adding indexController-adding index.blade.add index route to web.php

---
+++ app/Http/Controllers/indexController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shadi
+ * Date: 2/12/2020
+ * Time: 7:41 PM
+ */
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use DB;
+
+
+class indexController extends Controller
+{
+
+
+    public function showAll()
+    {
+        $posts = DB::select('select * from posts');
+        return view('index',['posts' => $posts]);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,33 @@ Route::get('/artist/{id}', 'artistController@showArtist')->name('artist');
 
 Route::get('/artists', 'artistController@showArtists')->name('artists');
 
-Route::post('/artist/likePost', 'artistController@checkLikeStatus')->name('likePost');
+Route::get('/', 'indexController@showAll')->name('posts');
 
 Route::post('/artist/unlikePost', 'artistController@checkunLikeStatus')->name('unlikePost');
+Route::post('/artist/likePost', 'artistController@checkLikeStatus')->name('likePost');
 
 Route::get('/userLogin', 'userController@signin');
 Route::get('/userLogin', 'userController@signup');
 Route::get('/userLogin/{id}', 'userController@favourites');
+
+Route::get('/login', 'loginController@showArtists')->name('login');
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/artists',function(){
+//     return view('artists');
+// })->name('artists');
+
+
+// Route::get('/artists',function(){
+//     return view('artists');
+// })->name('artists');
